fe8a61c8f50aee8f62cde38eb19c88c3fef194a0
planktonchik <planktonchik88@gmail.com>
2025-03-27 22:28:11 +0000

    Deploying to gh-pages from @ planktonchik/goit-js-hw-09@dd5d61e6b6782ca17b3d8f91ea5ad2a0894f3b46 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.querySelector("input");e.querySelector("textarea");
+//# sourceMappingURL=2-form.js.map
