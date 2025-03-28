9115ef345a3320f9eee733b8f8a509f7e9188fc7
Plankton <planktonchik88@gmail.com>
2025-03-28 09:38:43 -0600

    fix js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,38 @@
 const form = document.querySelector('.feedback-form');
-const formEmail = form.querySelector('input');
-const formMessage = form.querySelector('textarea');
 
 const localStorageKey = 'feedback-form-state';
 
-const formData = {
+let formData = {
   email: '',
   message: '',
 };
+
+window.addEventListener('load', () => {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+});
+
+form.addEventListener('input', event => {
+  if (event.target.name === 'email') {
+    formData.email = event.target.value.trim();
+  } else if (event.target.name === 'message') {
+    formData.message = event.target.value.trim();
+  }
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    formData = { email: '', message: '' };
+    form.reset();
+  }
+});
